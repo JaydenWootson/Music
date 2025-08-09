b416eac76aeef6a6aadd2cf72842f79f3b68e587
JaydenWootson <156025825+JaydenWootson@users.noreply.github.com>
2025-08-08 20:33:02 -0400

    Initialize project and enable Git version control

---
+++ src/Album.java
@@ -0,0 +1,21 @@
+import java.util.List;
+
+public class Album {
+    private int id;
+    private List<String> artists;
+    private String title;
+    private int numSongs;
+
+    public Album(int id, List<String> artists, String title, int numSongs) {
+        this.id = id;
+        this.artists = artists;
+        this.title = title;
+        this.numSongs = numSongs;
+    }
+
+    public int getNumSongs() {
+        return numSongs;
+    }
+
+
+}
