111a26efda6be876700f0922ca28dd7e3354c33e
JaydenWootson <156025825+JaydenWootson@users.noreply.github.com>
2025-08-08 20:52:30 -0400

    Implement append method with 0(1)

--- src/DoubleLinkedList.java
+++ src/DoubleLinkedList.java
@@ -9,4 +9,16 @@ public class DoubleLinkedList<T extends Comparable<T>> {
     private int size = 0;
 
     public void DoublyLinkedList() {}
+
+    public Node append(T data) {
+        Node newNode = new Node(data);
+        if (tail == null) head = tail = newNode;
+        else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+        size++;
+        return head;
+    }
 }
